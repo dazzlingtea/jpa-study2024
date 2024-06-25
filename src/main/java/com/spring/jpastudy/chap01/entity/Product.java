d845c2c4bfea67f1976ad3009fe67d3c5cff54bc
dazzlingtea <kitsnr08@gmail.com>
2024-06-25 10:50:45 +0900

    # JPA Entity, Repository, Save Test 생성

--- src/main/java/com/spring/jpastudy/chap01/entity/Product.java
+++ src/main/java/com/spring/jpastudy/chap01/entity/Product.java
@@ -1,21 +1,50 @@
 package com.spring.jpastudy.chap01.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @ToString
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 @Table(name="tbl_product")
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="prod_id")
     private Long id; // PK
 
-    @Column(name="prod_nm")
+    @Column(name="prod_nm", length=30, nullable = false)
     private String name; // 상품명
 
-    @Column(name="prod_price")
+    @Column(name="price")
     private int price; // 상품 가격
+
+    @Column(nullable = false)
+//    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
+    private Category category; // 상품 카테고리
+
+    @CreationTimestamp  // INSERT 시에 자동으로 서버시간 저장
+    @Column(updatable = false) // 수정불가
+    private LocalDateTime createdAt; // 상품 등록시간
+
+    @UpdateTimestamp  // UPDATE문 실행시 자동으로 시간이 저장
+    private LocalDateTime updatedAt; // 상품 수정시간
+
+    // 데이터베이스에는 저장 안하고 클래스 내부에서만 사용할 필드
+    @Transient
+    private String nickName;
+
+    public enum Category {
+        FOOD, FASHION, ELECTRONIC
+    }
 }
