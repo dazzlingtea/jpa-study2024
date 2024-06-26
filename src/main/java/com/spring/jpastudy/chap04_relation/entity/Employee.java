7bc825b663bc56d3f6ee70a5738de91da238c5d1
dazzlingtea <kitsnr08@gmail.com>
2024-06-26 11:00:39 +0900

    # JPA 단방향 연관관계 매핑

--- src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
+++ src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Getter @Setter
+@ToString
 @EqualsAndHashCode(of="id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,5 +22,10 @@ public class Employee {
 
     @Column(name = "emp_name", nullable = false)
     private String name;
+
+    // 단방향 매핑 - 데이터베이스처럼 한쪽에 상대방의 PK를 FK로 갖는 형태
+    @ManyToOne// Employee 기준으로 사원N:부서1이라 ManyToOne
+    @JoinColumn(name="dept_id") // FK 컬럼명
+    private Department department;
 
 }
