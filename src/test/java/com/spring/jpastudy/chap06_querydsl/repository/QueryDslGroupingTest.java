6caa99c70c313954cc2322cd15df1aa9026539c1
dazzlingtea <kitsnr08@gmail.com>
2024-06-28 11:45:40 +0900

    # JPA QueryDsl groupby 연습문제, DTO 처리 - Projections

--- src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslGroupingTest.java
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslGroupingTest.java
@@ -1,7 +1,12 @@
 package com.spring.jpastudy.chap06_querydsl.repository;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpastudy.chap06_querydsl.dto.GroupAverageAgeDto;
 import com.spring.jpastudy.chap06_querydsl.entity.Group;
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
 import org.junit.jupiter.api.BeforeEach;
@@ -107,7 +112,118 @@ class QueryDslGroupingTest {
 //        System.out.println("idolList = " + idolList);
         System.out.println("\n\n");
     }
+    @Test
+    @DisplayName("연령대별로 그룹화하여 아이돌 수를 조회")
+    void ageGroupTest() {
+        /*
+            SELECT
+                CASE age WHEN BETWEEN 10 AND 19 THEN 10
+                CASE age WHEN BETWEEN 20 AND 29 THEN 20
+                CASE age WHEN BETWEEN 30 AND 39 THEN 30
+                END,
+                COUNT(idol_id)
+            FROM tbl_idol
+            GROUP BY
+                CASE age WHEN BETWEEN 10 AND 19 THEN 10
+                CASE age WHEN BETWEEN 20 AND 29 THEN 20
+                CASE age WHEN BETWEEN 30 AND 39 THEN 30
+                END
 
+         */
+
+        //given
+
+        // QueryDSL로 CASE WHEN THEN 표현식 만들기
+        NumberExpression<Integer> ageGroupExpression = new CaseBuilder()
+                .when(idol.age.between(10, 19)).then(10)
+                .when(idol.age.between(20, 29)).then(20)
+                .when(idol.age.between(30, 39)).then(30)
+                .otherwise(0); // .as() 별칭
+
+        //when
+        List<Tuple> result = factory
+                .select(ageGroupExpression, idol.count())
+                .from(idol)
+                .groupBy(ageGroupExpression)
+                .having(idol.count().gt(5))
+                .fetch();
+
+        //then
+        assertFalse(result.isEmpty());
+        for (Tuple tuple : result) {
+            int ageGroupValue = tuple.get(ageGroupExpression);
+            long count = tuple.get(idol.count());
+
+            System.out.println("\n\nAge Group: " + ageGroupValue + "대, Count: " + count);
+        }
+    }
+    // 아이돌 그룹별로 아이돌의 그룹명과 평균 나이를 조회하세요.
+    // 평균 나이가 20세와 25세 사이인 그룹만 조회합니다.
+    @Test
+    @DisplayName("그룹별 평균 나이 조회")
+    void groupAvgAgeTest() {
+        //given
+        /*
+            SELECT G.group_name, AVG(I.age)
+            FROM tbl_idol I
+            JOIN tbl_group G
+            on I.group_id = G.group_id
+            GROUP BY G.group_id
+            HAVING AVG(I.age) BETWEEN 20 AND 25
+         */
+        //when
+        List<Tuple> result = factory.select(idol.group.groupName, idol.age.avg())
+                .from(idol)   // idol 안에 group이 있어서 idol만 사용해도 조인 가능
+                .groupBy(idol.group) // lazyloading 으로 조인
+                .having(idol.age.avg().between(20, 25))
+                .fetch();
+
+        //then
+        assertFalse(result.isEmpty());
+        for (Tuple tuple : result) {
+            String groupName = tuple.get(idol.group.groupName);
+            double averageAge = tuple.get(idol.age.avg());
+
+            System.out.println("\n\nGroup: " + groupName + ", Average Age: " + averageAge);
+        }
+    }
+
+    @Test
+    @DisplayName("그룹별 평균 나이 조회(결과 DTO 처리)")
+    void groupAvgAgeDtoTest() {
+        //given
+        /*
+            SELECT G.group_name, AVG(I.age)
+            FROM tbl_idol I
+            JOIN tbl_group G
+            on I.group_id = G.group_id
+            GROUP BY G.group_id
+            HAVING AVG(I.age) BETWEEN 20 AND 25
+         */
+        //when  -- 튜플 대신 커스텀 DTO
+        // Projections: 커스텀 DTO를 포장해주는 객체
+        List<GroupAverageAgeDto> result = factory
+                .select(
+                        Projections.constructor(
+                                GroupAverageAgeDto.class,
+                                idol.group.groupName,
+                                idol.age.avg()
+                        )
+                )
+                .from(idol)   // idol 안에 group이 있어서 idol만 사용해도 조인 가능
+                .groupBy(idol.group) // lazyloading 으로 조인
+                .having(idol.age.avg().between(20, 25))
+                .fetch();
+
+        //then
+        assertFalse(result.isEmpty());
+        for (GroupAverageAgeDto dto : result) {
+            String groupName = dto.getGroupName();
+            double averageAge = dto.getAverageAge();
+
+            System.out.println("\n\nGroup: " + groupName + ", Average Age: " + averageAge);
+        }
+    }
 
 
 }
