b71951a55274f04a5b40a38bc312a5d12756327d
dazzlingtea <kitsnr08@gmail.com>
2024-06-28 09:07:45 +0900

    # JPA QueryDsl Sort 세팅

---
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslSortTest.java
@@ -0,0 +1,63 @@
+package com.spring.jpastudy.chap06_querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpastudy.chap06_querydsl.entity.Group;
+import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static com.spring.jpastudy.chap06_querydsl.entity.QIdol.idol;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+//@Rollback(value = false)
+class QueryDslSortTest {
+
+    @Autowired
+    IdolRepository idolRepository;
+
+    @Autowired
+    GroupRepository groupRepository;
+
+    @Autowired
+    JPAQueryFactory factory;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        Group leSserafim = new Group("르세라핌");
+        Group ive = new Group("아이브");
+
+        groupRepository.save(leSserafim);
+        groupRepository.save(ive);
+
+        Idol idol1 = new Idol("김채원", 24, leSserafim);
+        Idol idol2 = new Idol("사쿠라", 26, leSserafim);
+        Idol idol3 = new Idol("가을", 22, ive);
+        Idol idol4 = new Idol("리즈", 20, ive);
+        Idol idol5 = new Idol("장원영", 20, ive);
+
+        idolRepository.save(idol1);
+        idolRepository.save(idol2);
+        idolRepository.save(idol3);
+        idolRepository.save(idol4);
+        idolRepository.save(idol5);
+
+    }
+
+    
+    
+
+
+
+}
