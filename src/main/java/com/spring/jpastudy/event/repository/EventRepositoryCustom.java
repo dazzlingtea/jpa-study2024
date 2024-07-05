ae0269fd64471c2ab0f62528851e1e8e3a860650
dazzlingtea <kitsnr08@gmail.com>
2024-07-05 10:57:23 +0900

    # 이벤트 api - JPA, QueryDSL 전체조회 페이징 추가

--- src/main/java/com/spring/jpastudy/event/repository/EventRepositoryCustom.java
+++ src/main/java/com/spring/jpastudy/event/repository/EventRepositoryCustom.java
@@ -1,12 +1,14 @@
 package com.spring.jpastudy.event.repository;
 
 import com.spring.jpastudy.event.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface EventRepositoryCustom {
 
-    List<Event> findEvents(String sort);
+    Page<Event> findEvents(Pageable pageable, String sort);
 
     // ... 필요한 거 작성하면 됨 -> JPA 리포지토리에 상속
 
